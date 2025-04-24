b658386638f60e79a38d6c80a61023b8830d7fc0
abdul hannan <hannanmughal007@gmail.com>
2025-04-25 00:22:25 +0500

    gdpr added

---
+++ src/assets/hotelImages.js
@@ -0,0 +1,252 @@
+// Hotel Dadou Images
+import hotelDadou from './Hotel-Dadou/Hotel-Dadou.png';
+import hotelDadouHero from './Hotel-Dadou/Hotel-Dadou-HERO.png';
+import hotelDadouLocation from './Hotel-Dadou/Hotel-Dadou-Location.png';
+import hotelDadouSlider1 from './Hotel-Dadou/Hotel-Dadou-Slider-1.png';
+import hotelDadouSlider2 from './Hotel-Dadou/Hotel-Dadou-Slider-2.png';
+import hotelDadouSlider3 from './Hotel-Dadou/Hotel-Dadou-Slider-3.png';
+import hotelDadouSlider4 from './Hotel-Dadou/Hotel-Dadou-Slider-4.png';
+import hotelDadouRooms1 from './Hotel-Dadou/Hotel-Dadou-ROOMS-1.png';
+import hotelDadouRooms2 from './Hotel-Dadou/Hotel-Dadou-ROOMS-2.png';
+import hotelDadouRooms3 from './Hotel-Dadou/Hotel-Dadou-ROOMS-3.png';
+import hotelDadouRooms4 from './Hotel-Dadou/Hotel-Dadou-ROOMS-4.png';
+import hotelDadouRooms5 from './Hotel-Dadou/Hotel-Dadou-ROOMS-5.png';
+import hotelDadouFacilities1 from './Hotel-Dadou/Hotel-Dadou-FACILITIES-1.png';
+import hotelDadouFacilities2 from './Hotel-Dadou/Hotel-Dadou-FACILITIES-2.png';
+import hotelDadouFacilities3 from './Hotel-Dadou/Hotel-Dadou-FACILITIES-3.png';
+import hotelDadouFacilities4 from './Hotel-Dadou/Hotel-Dadou-FACILITIES-4.png';
+import hotelDadouFacilities5 from './Hotel-Dadou/Hotel-Dadou-FACILITIES-5.png';
+
+// Monsieur Georges Images
+import monsieurGeorges from './Monsieur-Georges/Monsieur-Georges.png';
+import monsieurGeorgesHero from './Monsieur-Georges/Monsieur-Georges-HERO.png';
+import monsieurGeorgesLocation from './Monsieur-Georges/Monsieur-Georges-Location.png';
+import monsieurGeorgesSlider1 from './Monsieur-Georges/Monsieur-Georges-Slider-1.png';
+import monsieurGeorgesSlider2 from './Monsieur-Georges/Monsieur-Georges-Slider-2.png';
+import monsieurGeorgesSlider3 from './Monsieur-Georges/Monsieur-Georges-Slider-3.png';
+import monsieurGeorgesSlider4 from './Monsieur-Georges/Monsieur-Georges-Slider-4.png';
+import monsieurGeorgesRooms1 from './Monsieur-Georges/Monsieur-Georges-ROOMS-1.png';
+import monsieurGeorgesRooms2 from './Monsieur-Georges/Monsieur-Georges-ROOMS-2.png';
+import monsieurGeorgesRooms3 from './Monsieur-Georges/Monsieur-Georges-ROOMS-3.png';
+import monsieurGeorgesRooms4 from './Monsieur-Georges/Monsieur-Georges-ROOMS-4.png';
+import monsieurGeorgesFacilities1 from './Monsieur-Georges/Monsieur-Georges-FACILITIES-1.png';
+import monsieurGeorgesFacilities2 from './Monsieur-Georges/Monsieur-Georges-FACILITIES-2.png';
+import monsieurGeorgesFacilities3 from './Monsieur-Georges/Monsieur-Georges-FACILITIES-3.png';
+import monsieurGeorgesFacilities4 from './Monsieur-Georges/Monsieur-Georges-FACILITIES-4.png';
+
+// La Demeure Montaigne Images
+import laDemeureMontaigne from './La-Demeure-Montaigne/La-Demeure-Montaigne.png';
+import laDemeureMontaigneHero from './La-Demeure-Montaigne/La-Demeure-Montaigne-Hero.png';
+import laDemeureMontaigneLocation from './La-Demeure-Montaigne/La-Demeure-Montaigne-Location.png';
+import laDemeureMontaigneSlider1 from './La-Demeure-Montaigne/La-Demeure-Montaigne-Slider-1.png';
+import laDemeureMontaigneSlider2 from './La-Demeure-Montaigne/La-Demeure-Montaigne-Slider-2.png';
+import laDemeureMontaigneSlider3 from './La-Demeure-Montaigne/La-Demeure-Montaigne-Slider-3.png';
+import laDemeureMontaigneSlider4 from './La-Demeure-Montaigne/La-Demeure-Montaigne-Slider-4.png';
+import laDemeureMontaigneSlider5 from './La-Demeure-Montaigne/La-Demeure-Montaigne-Slider-5.png';
+import laDemeureMontaigneRooms1 from './La-Demeure-Montaigne/La-Demeure-Montaigne-ROOMS-1.png';
+import laDemeureMontaigneRooms2 from './La-Demeure-Montaigne/La-Demeure-Montaigne-ROOMS-2.png';
+import laDemeureMontaigneRooms3 from './La-Demeure-Montaigne/La-Demeure-Montaigne-ROOMS-3.png';
+import laDemeureMontaigneRooms4 from './La-Demeure-Montaigne/La-Demeure-Montaigne-ROOMS-4.png';
+import laDemeureMontaigneRooms5 from './La-Demeure-Montaigne/La-Demeure-Montaigne-ROOMS-5.png';
+import laDemeureMontaigneFacilities1 from './La-Demeure-Montaigne/La-Demeure-Montaigne-FACILITIES-1.png';
+import laDemeureMontaigneFacilities2 from './La-Demeure-Montaigne/La-Demeure-Montaigne-FACILITIES-2.png';
+import laDemeureMontaigneFacilities3 from './La-Demeure-Montaigne/La-Demeure-Montaigne-FACILITIES-3.png';
+import laDemeureMontaigneFacilities4 from './La-Demeure-Montaigne/La-Demeure-Montaigne-FACILITIES-4.png';
+import laDemeureMontaigneFacilities5 from './La-Demeure-Montaigne/La-Demeure-Montaigne-FACILITIES-5.png';
+
+// Hotel Victor Hugo Kleber Images
+import hotelVictorHugoKleber from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber.png';
+import hotelVictorHugoKleberHero from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-HERO.png';
+import hotelVictorHugoKleberLocation from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-Location.png';
+import hotelVictorHugoKleberSlider1 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-Slider-1.png';
+import hotelVictorHugoKleberSlider2 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-Slider-2.png';
+import hotelVictorHugoKleberSlider3 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-Slider-3.png';
+import hotelVictorHugoKleberSlider4 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-Slider-4.png';
+import hotelVictorHugoKleberSlider5 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-Slider-5.png';
+import hotelVictorHugoKleberRooms1 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-ROOMS-1.png';
+import hotelVictorHugoKleberRooms2 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-ROOMS-2.png';
+import hotelVictorHugoKleberRooms3 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-ROOMS-3.png';
+import hotelVictorHugoKleberRooms4 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-ROOMS-4.png';
+import hotelVictorHugoKleberFacilities1 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-FACILITIES-1.png';
+import hotelVictorHugoKleberFacilities2 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-FACILITIES-2.png';
+import hotelVictorHugoKleberFacilities3 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-FACILITIES-3.png';
+import hotelVictorHugoKleberFacilities4 from './Hotel-Victor-Hugo-Kleber/Hotel-Victor-Hugo-Kleber-FACILITIES-4.png';
+
+// Hotel Pastel Images
+import hotelPastel from './Hotel-Pastel/Hotel-Pastel.png';
+import hotelPastelHero from './Hotel-Pastel/Hotel-Pastel-HERO.png';
+import hotelPastelLocation from './Hotel-Pastel/Hotel-Pastel-Location.png';
+import hotelPastelSlider1 from './Hotel-Pastel/Hotel-Pastel-Slider-1.png';
+import hotelPastelSlider2 from './Hotel-Pastel/Hotel-Pastel-Slider-2.png';
+import hotelPastelSlider3 from './Hotel-Pastel/Hotel-Pastel-Slider-3.png';
+import hotelPastelSlider4 from './Hotel-Pastel/Hotel-Pastel-Slider-4.png';
+import hotelPastelRooms1 from './Hotel-Pastel/Hotel-Pastel-ROOMS-1.png';
+import hotelPastelRooms2 from './Hotel-Pastel/Hotel-Pastel-ROOMS-2.png';
+import hotelPastelRooms3 from './Hotel-Pastel/Hotel-Pastel-ROOMS-3.png';
+import hotelPastelFacilities1 from './Hotel-Pastel/Hotel-Pastel-FACILITIES-1.png';
+import hotelPastelFacilities2 from './Hotel-Pastel/Hotel-Pastel-FACILITIES-2.png';
+import hotelPastelFacilities3 from './Hotel-Pastel/Hotel-Pastel-FACILITIES-3.png';
+
+// Hotel Padam Images
+import hotelPadam from './Hotel-Padam/Hotel-Padam.png';
+import hotelPadamHero from './Hotel-Padam/Hotel-Padam-HERO.png';
+import hotelPadamLocation from './Hotel-Padam/Hotel-Padam-Location.png';
+import hotelPadamSlider1 from './Hotel-Padam/Hotel-Padam-Slider-1.png';
+import hotelPadamSlider2 from './Hotel-Padam/Hotel-Padam-Slider-2.png';
+import hotelPadamSlider3 from './Hotel-Padam/Hotel-Padam-Slider-3.png';
+import hotelPadamSlider4 from './Hotel-Padam/Hotel-Padam-Slider-4.png';
+import hotelPadamRooms1 from './Hotel-Padam/Hotel-Padam-ROOMS-1.png';
+import hotelPadamRooms2 from './Hotel-Padam/Hotel-Padam-ROOMS-2.png';
+import hotelPadamRooms3 from './Hotel-Padam/Hotel-Padam-ROOMS-3.png';
+import hotelPadamFacilities1 from './Hotel-Padam/Hotel-Padam-FACILITIES-1.png';
+import hotelPadamFacilities2 from './Hotel-Padam/Hotel-Padam-FACILITIES-2.png';
+import hotelPadamFacilities3 from './Hotel-Padam/Hotel-Padam-FACILITIES-3.png';
+
+// Hotel Keppler Images
+import hotelKeppler from './Hotel-Keppler/Hotel-Keppler.png';
+import hotelKepplerHero from './Hotel-Keppler/Hotel-Keppler-HERO.png';
+import hotelKepplerLocation from './Hotel-Keppler/Hotel-Keppler-Location.png';
+import hotelKepplerSlider1 from './Hotel-Keppler/Hotel-Keppler-Slider-1.png';
+import hotelKepplerSlider2 from './Hotel-Keppler/Hotel-Keppler-Slider-2.png';
+import hotelKepplerSlider3 from './Hotel-Keppler/Hotel-Keppler-Slider-3.png';
+import hotelKepplerSlider4 from './Hotel-Keppler/Hotel-Keppler-Slider-4.png';
+import hotelKepplerSlider5 from './Hotel-Keppler/Hotel-Keppler-Slider-5.png';
+import hotelKepplerSlider6 from './Hotel-Keppler/Hotel-Keppler-Slider-6.png';
+import hotelKepplerRooms1 from './Hotel-Keppler/Hotel-Keppler-ROOMS-1.png';
+import hotelKepplerRooms2 from './Hotel-Keppler/Hotel-Keppler-ROOMS-2.png';
+import hotelKepplerRooms3 from './Hotel-Keppler/Hotel-Keppler-ROOMS-3.png';
+import hotelKepplerRooms4 from './Hotel-Keppler/Hotel-Keppler-ROOMS-4.png';
+import hotelKepplerRooms5 from './Hotel-Keppler/Hotel-Keppler-ROOMS-5.png';
+import hotelKepplerFacilities1 from './Hotel-Keppler/Hotel-Keppler-FACILITIES-1.png';
+import hotelKepplerFacilities2 from './Hotel-Keppler/Hotel-Keppler-FACILITIES-2.png';
+import hotelKepplerFacilities3 from './Hotel-Keppler/Hotel-Keppler-FACILITIES-3.png';
+import hotelKepplerFacilities4 from './Hotel-Keppler/Hotel-Keppler-FACILITIES-4.png';
+import hotelKepplerFacilities5 from './Hotel-Keppler/Hotel-Keppler-FACILITIES-5.png';
+
+// Export all images
+export {
+    // Hotel Dadou
+    hotelDadou,
+    hotelDadouHero,
+    hotelDadouLocation,
+    hotelDadouSlider1,
+    hotelDadouSlider2,
+    hotelDadouSlider3,
+    hotelDadouSlider4,
+    hotelDadouRooms1,
+    hotelDadouRooms2,
+    hotelDadouRooms3,
+    hotelDadouRooms4,
+    hotelDadouRooms5,
+    hotelDadouFacilities1,
+    hotelDadouFacilities2,
+    hotelDadouFacilities3,
+    hotelDadouFacilities4,
+    hotelDadouFacilities5,
+
+    // Monsieur Georges
+    monsieurGeorges,
+    monsieurGeorgesHero,
+    monsieurGeorgesLocation,
+    monsieurGeorgesSlider1,
+    monsieurGeorgesSlider2,
+    monsieurGeorgesSlider3,
+    monsieurGeorgesSlider4,
+    monsieurGeorgesRooms1,
+    monsieurGeorgesRooms2,
+    monsieurGeorgesRooms3,
+    monsieurGeorgesRooms4,
+    monsieurGeorgesFacilities1,
+    monsieurGeorgesFacilities2,
+    monsieurGeorgesFacilities3,
+    monsieurGeorgesFacilities4,
+
+    // La Demeure Montaigne
+    laDemeureMontaigne,
+    laDemeureMontaigneHero,
+    laDemeureMontaigneLocation,
+    laDemeureMontaigneSlider1,
+    laDemeureMontaigneSlider2,
+    laDemeureMontaigneSlider3,
+    laDemeureMontaigneSlider4,
+    laDemeureMontaigneSlider5,
+    laDemeureMontaigneRooms1,
+    laDemeureMontaigneRooms2,
+    laDemeureMontaigneRooms3,
+    laDemeureMontaigneRooms4,
+    laDemeureMontaigneRooms5,
+    laDemeureMontaigneFacilities1,
+    laDemeureMontaigneFacilities2,
+    laDemeureMontaigneFacilities3,
+    laDemeureMontaigneFacilities4,
+    laDemeureMontaigneFacilities5,
+
+    // Hotel Victor Hugo Kleber
+    hotelVictorHugoKleber,
+    hotelVictorHugoKleberHero,
+    hotelVictorHugoKleberLocation,
+    hotelVictorHugoKleberSlider1,
+    hotelVictorHugoKleberSlider2,
+    hotelVictorHugoKleberSlider3,
+    hotelVictorHugoKleberSlider4,
+    hotelVictorHugoKleberSlider5,
+    hotelVictorHugoKleberRooms1,
+    hotelVictorHugoKleberRooms2,
+    hotelVictorHugoKleberRooms3,
+    hotelVictorHugoKleberRooms4,
+    hotelVictorHugoKleberFacilities1,
+    hotelVictorHugoKleberFacilities2,
+    hotelVictorHugoKleberFacilities3,
+    hotelVictorHugoKleberFacilities4,
+
+    // Hotel Pastel
+    hotelPastel,
+    hotelPastelHero,
+    hotelPastelLocation,
+    hotelPastelSlider1,
+    hotelPastelSlider2,
+    hotelPastelSlider3,
+    hotelPastelSlider4,
+    hotelPastelRooms1,
+    hotelPastelRooms2,
+    hotelPastelRooms3,
+    hotelPastelFacilities1,
+    hotelPastelFacilities2,
+    hotelPastelFacilities3,
+
+    // Hotel Padam
+    hotelPadam,
+    hotelPadamHero,
+    hotelPadamLocation,
+    hotelPadamSlider1,
+    hotelPadamSlider2,
+    hotelPadamSlider3,
+    hotelPadamSlider4,
+    hotelPadamRooms1,
+    hotelPadamRooms2,
+    hotelPadamRooms3,
+    hotelPadamFacilities1,
+    hotelPadamFacilities2,
+    hotelPadamFacilities3,
+
+    // Hotel Keppler
+    hotelKeppler,
+    hotelKepplerHero,
+    hotelKepplerLocation,
+    hotelKepplerSlider1,
+    hotelKepplerSlider2,
+    hotelKepplerSlider3,
+    hotelKepplerSlider4,
+    hotelKepplerSlider5,
+    hotelKepplerSlider6,
+    hotelKepplerRooms1,
+    hotelKepplerRooms2,
+    hotelKepplerRooms3,
+    hotelKepplerRooms4,
+    hotelKepplerRooms5,
+    hotelKepplerFacilities1,
+    hotelKepplerFacilities2,
+    hotelKepplerFacilities3,
+    hotelKepplerFacilities4,
+    hotelKepplerFacilities5,
+};
